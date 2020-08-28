fc14cc2c7550a8a9d2329ce09893550d9087b168
Daniel B <danielbuompane@gmail.com>
2020-08-27 22:20:57 -0400

    Add .gitignore and update dev dependencies

--- src/app.js
+++ src/app.js
@@ -3,7 +3,11 @@ const express = require("express");
 const app = express();
 
 app.get("", (req, res) => {
-  res.send("Welcome to recipe tracker");
+  res.send("Welcome to recipe tracker. Raw turkey is disgusting");
+});
+
+app.get("/colleen", (req, res) => {
+  res.send("This is colleen's page");
 });
 
 app.listen(3000, () => {
