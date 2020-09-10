e2ca86d9f8e0c1f7e216d1c42ec5b7d073448f95
Daniel B <danielbuompane@gmail.com>
2020-09-09 22:07:44 -0400

    Add validation for user model

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const validator = require("validator");
 
 const connectionURL = "mongodb://127.0.0.1:27017";
 
@@ -11,22 +12,42 @@ mongoose.connect(`${connectionURL}/recipe-tracker-api`, {
 const User = mongoose.model("User", {
   name: {
     type: String,
+    trim: true,
+    default: "anonymous",
   },
   username: {
     type: String,
+    trim: true,
+    default: "anonymous",
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    validate(value) {
+      if (!validator.isEmail(value)) {
+        throw new Error("Please provide a valid email address");
+      }
+    },
+  },
+  password: {
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 7,
   },
 });
 
-User.updateOne(
-  {
-    name: "Daniel",
-  },
-  {
-    username: "",
-  }
-)
-  .then((result) => {
-    console.log(result);
+const user = new User({
+  name: "Stache",
+  email: "wubba@gmail.com",
+  password: "chaserabbits",
+});
+
+user
+  .save()
+  .then(() => {
+    console.log(user);
   })
   .catch((error) => {
     console.log(error);
