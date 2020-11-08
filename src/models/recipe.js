affec7fde0a1d803d34f401a9cebdba6524efbf5
Daniel B <danielbuompane@gmail.com>
2020-11-08 11:54:09 -0500

    update recipe schema

--- src/models/recipe.js
+++ src/models/recipe.js
@@ -6,10 +6,32 @@ const recipeSchema = new mongoose.Schema({
     required: true,
     trim: true,
   },
-  recipe: {
+  chef: {
     type: String,
     required: true,
     trim: true,
+  },
+  recipe: {
+    ingredients: [
+      {
+        ingredient: {
+          name: {
+            type: String,
+            required: true,
+            trim: true,
+          },
+          measurement: {
+            type: Number,
+            required: true,
+          },
+          unit: {
+            type: String,
+            requires: true,
+            trim: true,
+          },
+        },
+      },
+    ],
   },
 });
 
