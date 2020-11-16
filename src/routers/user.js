aa5ba688f13dc3f102201e8d4b813abdc67ec46c
Daniel B <danielbuompane@gmail.com>
2020-11-15 19:27:06 -0500

    clean update and delete routers

--- src/routers/user.js
+++ src/routers/user.js
@@ -61,56 +61,34 @@ router.get("/users/profile", auth, async (req, res) => {
   res.send(req.user);
 });
 
-router.get("/users/:id", async (req, res) => {
-  try {
-    const user = await User.findById(req.params.id);
-    if (!user) {
-      return res.status(404).send({ error: "User not found" });
-    }
-
-    res.send(user);
-  } catch (e) {
-    res.status(500).send(e);
-  }
-});
-
-router.patch("/users/:id", async (req, res) => {
+router.patch("/users/me", auth, async (req, res) => {
   const updates = Object.keys(req.body);
   const allowedUpdates = ["name", "username", "email", "password"];
   const isValidOperation = updates.every((update) =>
     allowedUpdates.includes(update)
   );
 
+  if (!isValidOperation) {
+    return res.status(400).send({ error: "Invalid update!" });
+  }
   try {
-    if (!isValidOperation) {
-      return res.status(400).send({ error: "Invalid update!" });
-    }
-
-    const user = await User.findByIdAndUpdate(req.params.id, req.body, {
-      new: true,
-      runValidators: true,
-      useFindAndModify: false,
+    updates.forEach((update) => {
+      req.user[update] = req.body[update];
     });
 
-    if (!user) {
-      return res.status(404).send();
-    }
+    await req.user.save();
 
-    res.send(user);
-  } catch (e) {
-    res.status(500).send();
+    res.send(req.user);
+  } catch (error) {
+    res.status(400).send(error);
   }
 });
 
-router.delete("/users/:id", async (req, res) => {
+router.delete("/users/me", auth, async (req, res) => {
   try {
-    const user = await User.findByIdAndDelete(req.params.id);
-    if (!user) {
-      return res.status(404).send({ error: "User not found" });
-    }
-
-    res.send(user);
-  } catch (e) {
+    await req.user.remove();
+    res.send(req.user);
+  } catch (error) {
     res.status(500).send();
   }
 });
